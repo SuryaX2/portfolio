12a4c5ae86941dc39b266f41d9082c8fa7fa624a
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2025-02-10 20:43:10 +0530

    feat: remove unused SVG files and add new SVG assets for improved UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,17 @@
+import Grid from "@/components/Grid";
 import { Hero } from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
-import { FaHome } from "react-icons/fa";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex flex-col items-center justify-center overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav
-          navItems={[
-            { name: "Home", link: "/", icon: <FaHome /> },
-            { name: "About", link: "#about" },
-            { name: "Projects", link: "#projects" },
-            { name: "Contact", link: "#contact" },
-          ]}
+          navItems={navItems}
         />
         <Hero />
+        <Grid/>
       </div>
     </main>
   );
--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import { BentoGrid, BentoGridItem } from './ui/BentoGrid';
+import { gridItems } from "@/data";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
 
 const Grid = () => {
     return (
-        <section id='about'>
-            <BentoGrid>
-            {
-                    [{ tittle: 'Tittle1', description: 'Desc1', id: 1 }]
-                    .map((item) => (
-                        <BentoGridItem
-                            id={item.id}
-                            key={item.id}
-                            title={item.tittle}
-                            description={item.description}
-                        />
-                    )
-            }
+        <section id="about">
+            <BentoGrid className="w-full py-20">
+                {gridItems.map((item, i) => (
+                    <BentoGridItem
+                        id={item.id}
+                        key={i}
+                        title={item.title}
+                        description={item.description}
+                        className={item.className}
+                        img={item.img}
+                        imgClassName={item.imgClassName}
+                        titleClassName={item.titleClassName}
+                        spareImg={item.spareImg}
+                    />
+                ))}
             </BentoGrid>
         </section>
-    )
+    );
 };
 
 export default Grid;
