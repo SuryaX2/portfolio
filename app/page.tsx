4acb9d4aa8f721c0a6d92984decb58f729dcd102
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2025-02-11 17:18:34 +0530

    feat: add Projects component and integrate it into the main page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Grid from "@/components/Grid";
 import { Hero } from "@/components/Hero";
+import Projects from "@/components/Projects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import { navItems } from "@/data";
 
@@ -7,11 +8,10 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 flex flex-col items-center justify-center overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav
-          navItems={navItems}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
-        <Grid/>
+        <Grid />
+        <Projects />
       </div>
     </main>
   );
