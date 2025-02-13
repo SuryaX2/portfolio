155107e3893c7faa9a1c9c39c2317984f5787b7f
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2025-02-13 15:13:00 +0530

    feat: add Footer component and integrate it into the main page layout; update FloatingNav styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Approach from "@/components/Approach";
 import Education from "@/components/Education";
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import { Hero } from "@/components/Hero";
 import Projects from "@/components/Projects";
@@ -8,7 +9,7 @@ import { navItems } from "@/data";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex flex-col items-center justify-center overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex flex-col items-center justify-center mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
@@ -16,6 +17,7 @@ export default function Home() {
         <Education />
         <Projects />
         <Approach />
+        <Footer />
       </div>
     </main>
   );
